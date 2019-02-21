bce28c05e0643fe57070f4373eb77239586f8621
mile <791144831@qq.com>
2019-02-22 00:35:34 +0800

    表格展开-三级权限

--- src/components/roles.vue
+++ src/components/roles.vue
@@ -6,6 +6,32 @@
     <el-button class="btn" type="primary">添加角色</el-button>
     <!-- 表格 -->
     <el-table :data="roles" style="width: 100%">
+      <el-table-column type="expand">
+        <template slot-scope="scope">
+          <!-- 行列 -->
+          <el-row class="level1" v-for="(item1,i) in scope.row.children" :key="item1.id">
+            <el-col :span="4">
+              <el-tag type="danger" closable>
+                {{item1.authName}}
+              </el-tag>
+              <i class="el-icon-arrow-right"></i>
+            </el-col>
+            <el-col :span="20">
+              <el-row class="level2" v-for="(item2,i) in item1.children" :key="item2.id">
+                <el-col :span="4">
+                  <el-tag type="success" closable>
+                    {{item2.authName}}
+                  </el-tag>
+                  <i class="el-icon-arrow-right"></i>
+                </el-col>
+                <el-col :span="20">
+                  <el-tag v-for="(item3,i) in item2.children" :key="item3.id" type="warning" closable>{{item3.authName}}</el-tag>
+                </el-col>
+              </el-row>
+            </el-col>
+          </el-row>
+        </template>
+      </el-table-column>
       <el-table-column type="index" label="#" width="160"></el-table-column>
       <el-table-column prop="roleName" label="角色名称" width="200"></el-table-column>
       <el-table-column prop="roleDesc" label="角色描述" width="200"></el-table-column>
@@ -31,9 +57,7 @@ export default {
     this.getRoles();
   },
   methods: {
-    showDiaSetRights(){
-
-    },
+    showDiaSetRights() {},
     // 获取角色
     async getRoles() {
       const res = await this.$http.get(`roles`);
@@ -52,4 +76,14 @@ export default {
 .btn {
   margin-top: 20px;
 }
+.level1,
+.level2 {
+  margin-bottom: 10px;
+}
 </style>
+// // 行列布局->for嵌套
+// // 问题：请在控制台输出以下代码
+//   *
+//  * * 
+// * * *
+// // for 嵌套->外层3次 里层5次
